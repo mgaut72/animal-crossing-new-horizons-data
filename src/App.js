fd15907c9a08fb6dfe6c680ddacd586642974079
Matt Gautreau <gautreau@amazon.com>
2020-03-31 21:58:43 -0700

    add search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import CreatureGrid from './CreatureGrid.js';
+import Fuse from "fuse.js";
+import React, { useState } from 'react';
+import CreatureGrid from './CreatureGrid';
+import Header from './Header';
 import './App.css';
 import bugs from "./bugs.js";
 import fish from "./fish.js";
 
-function App() {
-  return (
-    <CreatureGrid creatures={[...bugs, ...fish]}/>
-  );
+const allCreatures = [...bugs, ...fish];
+
+function fuse(searchList, searchVal) {
+  var opts = {
+    shouldSort: true,
+    keys: ["name"]
+  };
+  var fuse = new Fuse(searchList, opts);
+  var res = fuse.search(searchVal);
+  return res.map(x => x.item);
 }
 
-export default App;
+function filterAllCreatures(searchString) {
+  console.log(`search string: ${searchString}`)
+  return searchString === ""
+    ? allCreatures
+    : fuse(allCreatures, searchString);
+}
+
+export default function App() {
+  const [searchString, setSearchString] = useState("");
+
+  return (
+    <div>
+      <Header searchString={searchString} onSearchChange={setSearchString}/>
+      <CreatureGrid creatures={filterAllCreatures(searchString)}/>
+    </div>
+  );
+}
